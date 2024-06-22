fe6922b4d4f01ce218eeb3b4fc47ae68297f59d4
selwavalencia@gmail.com <selwavalencia@gmail.com>
2024-06-22 12:24:57 -0400

    PP003: Implemented tokenize function.

--- ai_model/preprocessing/data_preprocessing.py
+++ ai_model/preprocessing/data_preprocessing.py
@@ -1,6 +1,11 @@
 # Preprocessing
 import pandas as pd
 import re
+import nltk
+from nltk.tokenize import word_tokenize
+
+# Downloads
+nltk.download('punkt')
 
 data = 'ai_model\preprocessing\SentimentTrain.csv'
 df = pd.read_csv(data)
@@ -11,8 +16,15 @@ def remove_hashtags(text):
     text = re.sub(r'^\#(\w+)\b\s*', r'\1', text)
     return text
 
+def tokenize(text):
+    return word_tokenize(text)
+
 # Delete Hashtags
 df['text'] = df['text'].apply(remove_hashtags)
 print("No HASH \n", df['text'].iloc[11])
 print("No HASH \n", df['text'].iloc[64])
 print("No HASH \n", df['text'].iloc[36])
+
+# Tokenization
+df['tokens'] = df['text'].apply(tokenize)
+print("All Tokens: \n", df['tokens'].iloc[1])
