aae057e8b60b47582e0837cc2402ab4bd1a2c3dc
selwavalencia@gmail.com <selwavalencia@gmail.com>
2024-06-22 12:18:42 -0400

    PP003: Implemented remove_hashtags function.

--- ai_model/preprocessing/data_preprocessing.py
+++ ai_model/preprocessing/data_preprocessing.py
@@ -1,5 +1,18 @@
 # Preprocessing
 import pandas as pd
+import re
 
-data = '.\SentimentTrain.csv'
+data = 'ai_model\preprocessing\SentimentTrain.csv'
 df = pd.read_csv(data)
+
+def remove_hashtags(text):
+    text = re.sub(r'\s*#\w+\b\s*$', '', text)
+    text = re.sub(r'\b\s*#(\w+)', r'\1', text)
+    text = re.sub(r'^\#(\w+)\b\s*', r'\1', text)
+    return text
+
+# Delete Hashtags
+df['text'] = df['text'].apply(remove_hashtags)
+print("No HASH \n", df['text'].iloc[11])
+print("No HASH \n", df['text'].iloc[64])
+print("No HASH \n", df['text'].iloc[36])
