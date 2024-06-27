575dda10bea4ae434fca89998291de5db6ffbc57
selwavalencia@gmail.com <selwavalencia@gmail.com>
2024-06-27 02:50:13 -0400

    init002: Implemented validations to input textarea.

--- twitter_app/src/app/home/home.component.ts
+++ twitter_app/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, Injectable } from '@angular/core';
-import { FormsModule, NgModel, ReactiveFormsModule}   from '@angular/forms';
+import { Component, Injectable} from '@angular/core';
+import { FormsModule, NgModel, ReactiveFormsModule, FormBuilder, FormControl, Validators}   from '@angular/forms';
 import { Tweet } from '../interfaces/tweet';
 import { CommonModule } from '@angular/common';
 import { TweetComponent } from '../tweet/tweet.component';
@@ -9,9 +9,9 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatFormFieldModule, MatFormFieldControl} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
-
 import {environment} from '../../environment';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { isEmpty } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -36,16 +36,20 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  
-  constructor(
-    private http: HttpClient
-  ){}
 
   tweetsList: Tweet[] = [];
   filteredTweets: Tweet[] = [];
   tweetContent: string = '';
-  contentInput: string = '';
   selectedSentiment: number = 0;
+
+  contentInput = new FormControl('', [
+    Validators.required,
+    this.validate_input.bind(this)
+  ]);
+
+  constructor(
+    private http: HttpClient
+  ){}
 
   ngOnInit() {
     this.get_all_tweets();
@@ -67,12 +71,35 @@ export class HomeComponent {
       }
   
       this.tweetsList.unshift(newTweet);
-      this.tweetContent = this.contentInput;
-      this.contentInput = '';
+      this.contentInput.reset();
       console.log("Posted Tweets: ", saved_tweet);
     });
   }
 
+  validate_input(control: FormControl): { [key: string]: any } | null {
+    const input_value = control.value;
+    if (!input_value || this.is_empty(input_value) || !this.count_words(input_value) || !this.contains_letters(input_value)) {
+      return { 'validate_input': true };
+    }
+    return null;
+  }
+
+  is_empty(text: string): boolean {
+    return text.trim().length === 0;
+  }
+
+  count_words(text: string): boolean {
+    const min_words = 2;
+    const words = text.trim().split(' ');
+    const filter_blanc_words = words.filter(word => word !== '');
+    return filter_blanc_words.length >= min_words;
+  }
+
+  contains_letters(text: string): boolean {
+    const regex = /[a-zA-Z]/
+    return regex.test(text)
+  }
+  
   get_all_tweets() {
     this.http.get<Tweet[]>(`${environment.apiUrl}/tweets`).subscribe((tweets) => {
       console.log("Id: ", tweets);
